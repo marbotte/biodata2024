03852b76a70abbd5920e6b14e48a62815ae6d18a
marbotte <bottinmarius@gmail.com>
2024-12-06 17:01:20 -0500

    organized missing pages

--- r_func/render_actividades.R
+++ r_func/render_actividades.R
@@ -1,3 +1,4 @@
 require(rmarkdown)
 require(knitr)
 rmarkdown::render("act_1_1_SQL.Rmd",output_format=bookdown::pdf_document2(),output_dir = "./docs/download/actividades/taller_R_SQL/")
+rmarkdown::render("act_1_2_R.Rmd",output_format=bookdown::pdf_document2(),output_dir = "./docs/download/actividades/taller_R_SQL/")
